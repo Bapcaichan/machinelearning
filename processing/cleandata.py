6cb80e87aec3ce0685ec86bbe54fe7eed1e363e6
Thanh <dodinhthanh2004@gmail.com>
2024-12-23 23:11:24 +0700

    change clean data

--- processing/cleandata.py
+++ processing/cleandata.py
@@ -5,11 +5,13 @@ import pandas as pd
 # df = pd.read_csv(r'../data/dataset_traffic_accident_prediction1.csv')
 df = pd.read_csv(r'C:\Users\ADMIN\Documents\VScode\Python\github\machinelearning\data\dataset_traffic_accident_prediction1.csv')
 
-df = df.drop(df['Accident_Severity'],axis=1)
 
-df1 = df['Road_Light_Condition'].fillna(df['Road_Light_Condition'].mode(), inplace = True)
-df1 = df['Road_Type'] = df['Road_Type'].fillna(df['Road_Type'].mode()[0])
-df1 = df['Driver_Alcohol'] = df['Driver_Alcohol'].fillna(df['Driver_Alcohol'].mode()[0])
+
+df1 = df.drop(["Accident_Severity"], axis =1 )
+
+df1['Road_Light_Condition'].fillna(df1['Road_Light_Condition'].mode()[0], inplace=True)
+df1['Road_Type'].fillna(df['Road_Type'].mode()[0], inplace= True)
+df1['Driver_Alcohol'].fillna(df['Driver_Alcohol'].mode()[0], inplace= True)
 
 
 
@@ -26,3 +28,4 @@ nan = df1.isnull().sum()
 
 print(df1["Weather"].unique())
 test = df1.dropna()
+print(test)
--- processing/tempCodeRunnerFile.py
+++ processing/tempCodeRunnerFile.py
@@ -1,8 +1,1 @@
-import numpy as np
-import pandas as pd
-
-df = pd.read_csv(r'C:\Users\ADMIN\Documents\VScode\Python\github\machinelearning\data\dataset_traffic_accident_prediction1.csv')
-
-print(df)
-
 
