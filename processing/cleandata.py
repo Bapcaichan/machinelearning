58afa653c0d42d6013dc322eacb66b350d47bc15
henshin <120329362+Bapcaichan@users.noreply.github.com>
2024-12-30 00:52:58 +0700

    Update cleandata.py

--- processing/cleandata.py
+++ processing/cleandata.py
@@ -1,31 +1,40 @@
+# thinh
+import operator
 
 import numpy as np
 import pandas as pd
+from ydata_profiling import ProfileReport
 
-# df = pd.read_csv(r'../data/dataset_traffic_accident_prediction1.csv')
-df = pd.read_csv(r'C:\Users\ADMIN\Documents\VScode\Python\github\machinelearning\data\dataset_traffic_accident_prediction1.csv')
-
-
+df = pd.read_csv('../data/dataset_traffic_accident_prediction1.csv')
 
 df1 = df.drop(["Accident_Severity"], axis =1 )
 
-df1['Road_Light_Condition'].fillna(df1['Road_Light_Condition'].mode()[0], inplace=True)
-df1['Road_Type'].fillna(df['Road_Type'].mode()[0], inplace= True)
-df1['Driver_Alcohol'].fillna(df['Driver_Alcohol'].mode()[0], inplace= True)
-
-
-
-
-
-
-
-# thinh
 out = df1.describe()
 
-nan = df1.isnull().sum()
+nan = df1.isnull().sum() # co nhieu gia tri nan khong cung 1 record
 
-# tinh thu gini index
+#xu ly cac hang co 2 gia tri null
+df2 = df1.dropna(thresh = 11)
+out1 = df2.describe()
 
-print(df1["Weather"].unique())
-test = df1.dropna()
-print(test)
+obj = df2.select_dtypes(include = 'object').columns
+
+#xu ly du lieu nominal bang tan suat
+for i in obj:
+
+    df2[i] = df2[i].fillna(df2[i].mode()[0])
+
+#xu ly du lieu so bang trung vi
+
+flt = df2.select_dtypes(include = 'float').columns
+
+for i in flt:
+    df2[i] = df2[i].fillna(df2[i].median())
+
+out2 = df2.isna().sum()
+
+# Bo du lieu trung lap
+df3 = df.drop_duplicates()
+
+# profile = ProfileReport(df3, title="Traffic accident", explorative=True)
+# profile.to_file("Traffic.html")
