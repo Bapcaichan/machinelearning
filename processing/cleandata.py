0393c759df29fcac8d1c81ec849cdbe6ad9c3a41
Thanh <dodinhthanh2004@gmail.com>
2025-01-06 18:36:06 +0700

    change cleandata.py

--- processing/cleandata.py
+++ processing/cleandata.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from ydata_profiling import ProfileReport
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # df = pd.read_csv('../data/dataset_traffic_accident_prediction1.csv')
 df = pd.read_csv(r'C:\Users\ADMIN\Documents\VScode\Python\github\machinelearning\data\dataset_traffic_accident_prediction1.csv')
@@ -38,7 +39,7 @@ df['Accident'].fillna(df['Accident'].median(), inplace=True)
 
 #Kiểm tra dữ liệu
 columns_miss = df.columns[df.isnull().sum() > 0]
-
+df[columns_miss].info()
 #Xử lý dữ liệu thiếu bằng trung bình
 df['Weather'] = df['Weather'].fillna(df['Weather'].mode()[0])
 df['Road_Type'] = df['Road_Type'].fillna(df['Road_Type'].mode()[0])
@@ -49,6 +50,51 @@ df['Vehicle_Type'] = df['Vehicle_Type'].fillna(df['Vehicle_Type'].mode()[0])
 df['Road_Light_Condition'] = df['Road_Light_Condition'].fillna(df['Road_Light_Condition'].mode()[0])
 #Kiểm tra dữ liệu thiếu
 df.isnull().sum()
+
+
+#Kiểm tra bản ghi trùng lặp
+duplicates_count = df.duplicated().sum()
+print(f"Ban ghi trung lap: {duplicates_count}")
+#Xóa các bản ghi trùng lặp
+df = df.drop_duplicates()
+duplicates_count1 = df.duplicated().sum()
+print(f"Ban ghi trung lap: {duplicates_count1}")
+
+#Kiểm tra ngoại lệ
+
+# Vẽ biểu đồ boxplot cho Speed_Limit
+plt.figure(figsize=(8, 6))
+sns.boxplot(x=df['Speed_Limit'])
+plt.title('Speed_Limit')
+plt.show()
+
+# Vẽ biểu đồ boxplot cho Number_of_Vehicles
+plt.figure(figsize=(8, 6))
+sns.boxplot(x=df['Number_of_Vehicles'])
+plt.title('Number_of_Vehicles')
+plt.show()
+
+# Vẽ biểu đồ boxplot cho Driver_Age
+plt.figure(figsize=(8, 6))
+sns.boxplot(x=df['Driver_Age'])
+plt.title('Driver_Age')
+plt.show()
+
+# Vẽ biểu đồ boxplot cho Driver_Experience
+plt.figure(figsize=(8, 6))
+sns.boxplot(x=df['Driver_Experience'])
+plt.title('Driver_Experience')
+plt.show()
+
+# Vẽ biểu đồ boxplot cho Traffic_Density
+plt.figure(figsize=(8, 6))
+sns.boxplot(x=df['Traffic_Density'])
+plt.title('Traffic_Density')
+plt.show()
+
+cols = ['Speed_Limit', 'Number_of_Vehicles', 'Driver_Age', 'Driver_Experience']
+sns_plot = sns.pairplot(df[cols])
+plt.show()
 
 # with open('../data/input.csv', mode='w', newline='', encoding='utf-8') as file: writer = csv.writer(file) 
 # writer.writerows(df3)
--- processing/tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-df = pd.read_csv(r'C:\Users\ADMIN\Documents\VScode\Python\github\machinelearning\data\dataset_traffic_accident_prediction1.csv')
